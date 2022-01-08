c25d1158f666db8f6501094ec48988210534e591
Zack <lorenx@163.com>
2022-01-08 14:50:11 +0800

    chore: update docs

--- .umirc.ts
+++ .umirc.ts
@@ -1,25 +1,17 @@
-import { IConfig } from 'umi-types';
+import { defineConfig } from 'dumi';
 
-// ref: https://umijs.org/config/
-const config: IConfig = {
-  treeShaking: true,
-  plugins: [
-    // ref: https://umijs.org/plugin/umi-plugin-react.html
-    [
-      'umi-plugin-react',
-      {
-        antd: false,
-        dva: false,
-        dynamicImport: false,
-        title: 'umijs',
-        dll: false,
-
-        routes: {
-          exclude: [/components\//],
-        },
-      },
-    ],
+export default defineConfig({
+  title: 'AoraJS',
+  mode: 'site',
+  outputPath: 'dist',
+  base: '/',
+  publicPath: '/',
+  navs: [
+    null,
+    {
+      title: 'GitHub',
+      path: 'https://github.com/aorajs/aora',
+    },
   ],
-};
-
-export default config;
+  polyfill: false,
+});
